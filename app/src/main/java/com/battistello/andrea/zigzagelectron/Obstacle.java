b1e70fda20c06e79d169ab948d9c9a6267dabc56
Andrea <zippa.batti94@gmail.com>
2015-12-15 22:18:56 +0100

    Added an empty constructor.

--- app/src/main/java/com/battistello/andrea/zigzagelectron/Obstacle.java
+++ app/src/main/java/com/battistello/andrea/zigzagelectron/Obstacle.java
@@ -7,11 +7,14 @@ import com.battistello.andrea.framework.math.Vector2;
  * Created by Andrea on 07/12/15.
  */
 public class Obstacle extends DynamicGameObject {
+    public Obstacle() {
+        super (0, 0, 0, 0);
+        velocity = new Vector2(0, World.SCROLL_SPEED_Y);
+    }
     public Obstacle(float x, float y, float width, float height) {
         super (x, y, width, height);
         this.velocity = new Vector2(0, World.SCROLL_SPEED_Y);
     }
-
 
     public void update(float deltaTime) {
         this.moveBy(velocity.x*deltaTime, velocity.y * deltaTime);
