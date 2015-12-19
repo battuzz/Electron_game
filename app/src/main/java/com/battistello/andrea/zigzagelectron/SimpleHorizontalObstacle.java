91c6f71925ee3a4b5577ae48c7d6155157c67f20
Andrea <zippa.batti94@gmail.com>
2015-12-19 15:51:17 +0100

    Now obstacles are like |##      ####|.

--- app/src/main/java/com/battistello/andrea/zigzagelectron/SimpleHorizontalObstacle.java
+++ app/src/main/java/com/battistello/andrea/zigzagelectron/SimpleHorizontalObstacle.java
@@ -13,37 +13,38 @@ import java.util.Random;
  * Created by Andrea on 15/12/15.
  */
 public class SimpleHorizontalObstacle extends Obstacle {
+    public final float OBS_HEIGHT = 50.0f;
+    public final float aperture_size = World.WORLD_WIDTH / 4;    // Width of the space between the two obstacles where the main electron should pass through.
     float height;
-    DynamicGameObject obstacle;
+//    DynamicGameObject obstacle;
+
+    DynamicGameObject obstacle1, obstacle2;
 
 
     Random r;
     public SimpleHorizontalObstacle(float starting_height) {
         r = new Random();
-        float x = random_x();
-        float width = 200.0f;
-        float h = 50.0f;
+        generateRandomObstacles(starting_height);
 
 
-        obstacle = new DynamicGameObject(x, starting_height, width, h);
-        obstacle.velocity.set(obsVelocity.x, obsVelocity.y);
+//        obstacle = new DynamicGameObject(x, starting_height, width, h);
+//        obstacle.velocity.set(obsVelocity.x, obsVelocity.y);
 
         height = 150.0f;
     }
 
-    private float random_x() {
-        boolean b = r.nextBoolean();
-        if (b) {
-            return 100.0f;
-        }
-        else {
-            return 220.0f;
-        }
+    private void generateRandomObstacles(float starting_height) {
+        float x_left = r.nextFloat() * (World.WORLD_WIDTH - aperture_size);    //The x coordinate of the left side of the aperture
+        float x_right = x_left + aperture_size;
+
+        obstacle1 = new DynamicGameObject(x_left - World.WORLD_WIDTH/2, starting_height, World.WORLD_WIDTH, OBS_HEIGHT);
+        obstacle2 = new DynamicGameObject(x_right + World.WORLD_WIDTH/2, starting_height, World.WORLD_WIDTH, OBS_HEIGHT);
     }
 
     @Override
     public Vector2 getPosition() {
-        return obstacle.position;
+//        return obstacle.position;
+        return obstacle1.position;
     }
 
     @Override
@@ -53,16 +54,21 @@ public class SimpleHorizontalObstacle extends Obstacle {
 
     @Override
     public void update(float deltaTime) {
-        obstacle.moveBy(obsVelocity.x * deltaTime, obsVelocity.y * deltaTime);
+//        obstacle.moveBy(obsVelocity.x * deltaTime, obsVelocity.y * deltaTime);
+        obstacle1.moveBy(obsVelocity.x * deltaTime, obsVelocity.y * deltaTime);
+        obstacle2.moveBy(obsVelocity.x * deltaTime, obsVelocity.y * deltaTime);
     }
 
     @Override
     public boolean collidesWith(Shape s) {
-        return OverlapTester.overlap(s, obstacle.bounds);
+//        return OverlapTester.overlap(s, obstacle.bounds);
+        return OverlapTester.overlap(s, obstacle1.bounds) || OverlapTester.overlap(s, obstacle2.bounds);
     }
 
     @Override
     public void render(SpriteBatcher b) {
-        b.drawSprite(obstacle.position.x, obstacle.position.y, obstacle.bounds.width(), obstacle.bounds.height(), Assets.obstacleRegion);
+//        b.drawSprite(obstacle.position.x, obstacle.position.y, obstacle.bounds.width(), obstacle.bounds.height(), Assets.obstacleRegion);
+        b.drawSprite(obstacle1.position.x, obstacle1.position.y, obstacle1.bounds.width(), obstacle1.bounds.height(), Assets.obstacleRegion);
+        b.drawSprite(obstacle2.position.x, obstacle2.position.y, obstacle2.bounds.width(), obstacle2.bounds.height(), Assets.obstacleRegion);
     }
 }
