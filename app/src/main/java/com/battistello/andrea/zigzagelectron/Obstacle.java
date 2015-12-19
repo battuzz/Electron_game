e43a5b075f39a28cb98abfd0b55032a3512a4cdb
Andrea <zippa.batti94@gmail.com>
2015-12-19 15:21:30 +0100

    Removed dependency from DynamicGameObject. Now Obstacle subclasses should manage the objects on their own.

--- app/src/main/java/com/battistello/andrea/zigzagelectron/Obstacle.java
+++ app/src/main/java/com/battistello/andrea/zigzagelectron/Obstacle.java
@@ -1,22 +1,28 @@
 package com.battistello.andrea.zigzagelectron;
 
 import com.battistello.andrea.framework.DynamicGameObject;
+import com.battistello.andrea.framework.gl.SpriteBatcher;
+import com.battistello.andrea.framework.math.Shape;
 import com.battistello.andrea.framework.math.Vector2;
 
 /**
  * Created by Andrea on 07/12/15.
  */
-public class Obstacle extends DynamicGameObject {
+public abstract class Obstacle {
+    Vector2 obsVelocity;
+
     public Obstacle() {
-        super (0, 0, 0, 0);
-        velocity = new Vector2(0, World.SCROLL_SPEED_Y);
-    }
-    public Obstacle(float x, float y, float width, float height) {
-        super (x, y, width, height);
-        this.velocity = new Vector2(0, World.SCROLL_SPEED_Y);
+        obsVelocity = new Vector2(0, World.SCROLL_SPEED_Y);
     }
 
-    public void update(float deltaTime) {
-        this.moveBy(velocity.x*deltaTime, velocity.y * deltaTime);
+    public void setVelocity(float vx, float vy) {
+        obsVelocity.set(vx, vy);
     }
+
+    public abstract float getHeight();
+    public abstract void update(float deltaTime);
+    public abstract boolean collidesWith(Shape s);
+    public abstract Vector2 getPosition();
+    public abstract void render(SpriteBatcher b);
+
 }
